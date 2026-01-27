7c10af9c89ed0a24cb887d4a8e00f769cf11f11e
kshhyun <tjgus8288508@gmail.com>
2026-01-27 13:36:53 +0900

    docs: add comments to index, hello template, and controller

--- hello-spring/src/main/java/Hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/Hello/hello_spring/controller/HelloController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello") //Web application에서 /hello을 누르면 이 method를 호출
-    public String hello(Model model){ //MVC에서 'M'에 해당
-        //'data'를 'hello'로 넘김
-        model.addAttribute("data","hello");
-        return "hello";
+    // 1. Web 실행 -> /hello를 누르면 아래 hello() method 호출
+    @GetMapping("hello")
+    public String hello(Model model){ //MVC에서 'M'에 해당 - model은 View에 전달할 데이터를 key=value로 담아 전달
+        //hello.html에 'data'를 'hello'로 넘김
+        model.addAttribute("data","hello!!");
+        return "hello"; //view의 이름은 return 값으로 표현 -> resources/templates/hello.html로 이동
     }
 }
